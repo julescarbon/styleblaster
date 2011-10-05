c19b9ce506faec7707ee77e18cbeb6c49c71474c
jgv <jvingiano@gmail.com>
2011-10-05 12:38:12 -0400

    barebones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoboothGallery::Application.config.secret_token = 'f90069d87c26c53e2eee00cf65f933a5b392fab65ceca7150da76588bcb19a0d3517e9216de2e93f8278576dfd0b027c579b07b5456fa8438eb8681770be68fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PhotoboothGallery::Application.config.session_store :cookie_store, key: '_photobooth-gallery_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PhotoboothGallery::Application.config.session_store :active_record_store
